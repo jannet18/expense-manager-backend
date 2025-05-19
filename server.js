18e9d08d48eb1a4d47ba436ceda6ce9e83ca3a51
jannet18 <janetngei18@gmail.com>
2025-05-19 17:49:07 +0300

    add:income, get all income, download income excel, delete income source routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes.js");
+const incomeRoutes = require("./routes/incomeRoutes.js");
+
 const app = express();
 
 app.use(
@@ -19,6 +21,8 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/v1/income", incomeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
